9264105977aa1af0e085cd1e9c9062752a7bebaf
vkpatva <19it117@charusat.edu.in>
2023-09-10 22:21:56 +0530

    aplloo client added

--- subgraph-tutorial/src/App.jsx
+++ subgraph-tutorial/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { Table } from '@mui/material';
 import { Deposit } from './components/Deposit/Deposit.component';
 import { Withdraw } from './components/Withdraw/Withdraw.component';
 function App() {
